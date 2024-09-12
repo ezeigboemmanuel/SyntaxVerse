5ddbed8c0920335b5ada98692d3fd6da65a8e975
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-09-12 08:13:52 +0100

    update: font, working on navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <label>Ztheta</label>
+      </div>
+      <div>
+        <div>Searcchbar</div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar

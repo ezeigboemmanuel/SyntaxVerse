79f653a9fcbbaf225455f5c9bd68ff980f6c7926
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-09-13 18:39:46 +0100

    Feat: tags state

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { Button } from "./ui/button";
+"use client"
+import React, { useState } from "react";
 
 const Tags = () => {
+  const [active, setActive] = useState("All Articles")
   const tags = [
     "All Articles",
     "Web Development",
@@ -14,7 +15,7 @@ const Tags = () => {
     <div className="pt-4">
       <div>
         {tags.map((tag) => (
-          <div key={tag} className="mr-2 mb-2 px-5 py-1.5 inline-flex text-xs md:text-base rounded-full border border-gray-600 text-gray-600 hover:bg-[#6C40FE] hover:text-white hover:border-white">{tag}</div>
+          <div key={tag} onClick={() => setActive(tag)} className={`mr-2 mb-2 px-5 py-1.5 inline-flex text-xs md:text-base rounded-full border border-gray-600 text-gray-600 hover:bg-[#6C40FE] hover:text-white hover:border-white cursor-pointer ${active == tag && "bg-[#6C40FE] text-white border-white"}`}>{tag}</div>
         ))}
       </div>
     </div>

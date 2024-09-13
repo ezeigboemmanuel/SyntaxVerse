dde832f90484af07c3f21deff9ffc98464fb4f39
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-09-13 18:09:28 +0100

    update: dashboard shadow

--- app/dashboard/_components/StatsCard.tsx
+++ app/dashboard/_components/StatsCard.tsx
@@ -6,9 +6,9 @@ const StatsCard = () => {
       <h1 className="uppercase text-2xl md:text-4xl font-bold">Your Stats</h1>
 
       <div className="mt-7">
-        <div className="grid grid-cols-1 md:grid-cols-3 bg-[#FCFCFE] rounded-lg shadow-md p-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 bg-[#FCFCFE] rounded-lg shadow-sm p-6">
           <div className="border-b md:border-r md:border-b-0 pb-2 md:pb-0 md:pr-2">
-            <h3 className="text-gray-700 text-[16px] font-semibold text-left">
+            <h3 className="text-gray-700 text-left">
               Total Articles
             </h3>
 
@@ -16,7 +16,7 @@ const StatsCard = () => {
           </div>
 
           <div className="pt-3 pb-3 md:pb-0 md:pt-0 md:pl-6">
-            <h3 className="text-gray-700 text-[16px] font-semibold text-left">
+            <h3 className="text-gray-700 text-left">
               Total Reads
             </h3>
 
@@ -24,7 +24,7 @@ const StatsCard = () => {
           </div>
 
           <div className="border-t md:border-l md:border-t-0 pt-3 md:pt-0 md:pl-6">
-            <h3 className="text-gray-700 text-[16px] font-semibold text-left">
+            <h3 className="text-gray-700 text-left">
               Total Likes
             </h3>
 

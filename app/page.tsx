b7713658d9282772b917b36451451bdd264608d0
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-09-19 16:31:14 +0100

    refactor: Got the blogs via props

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
+"use client"
 import BlogList from "@/components/BlogList";
 import Featured from "@/components/Featured";
 import Tags from "@/components/Tags";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 
 export default function Home() {
+  const blogs = useQuery(api.blogs.getAllBlogs, {});
+  if (blogs === undefined) {
+    return <p>Loading...</p>;
+  }
   return (
       <div className="pb-14 md:pb-20">
         <Tags />
         <Featured />
         <hr className="mx-4 md:mx-7" />
-        <BlogList />
+        <BlogList blogs={blogs} />
       </div>
   );
 }
--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,50 +1,63 @@
 "use client";
 import Image from "next/image";
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
 import { AspectRatio } from "./ui/aspect-ratio";
+import Link from "next/link";
+import { Id } from "@/convex/_generated/dataModel";
 
-const BlogList = () => {
-  const blogs = useQuery(api.blogs.getAllBlogs, {});
-  if (blogs === undefined) {
-    return <p>Loading...</p>;
-  }
+interface BlogListProps {
+  blogs: {
+    imageUrl: string;
+    _id: Id<"blogs">;
+    _creationTime: number;
+    likes?: number | undefined;
+    storageId: Id<"_storage">;
+    format: string;
+    userId: Id<"users">;
+    title: string;
+    article: string;
+    views: number;
+    categories: string[];
+  }[];
+}
+const BlogList = ({ blogs }: BlogListProps) => {
   return (
     <div className="pt-6 pb-6 md:pt-10 md:pb-10">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-5 w-full">
         {blogs.map((blog) => (
-          <div key={blog.title} className="group cursor-pointer">
-            <div>
-              <AspectRatio ratio={16 / 12}>
-                <Image
-                  src={blog.imageUrl}
-                  fill
-                  alt="blog_img"
-                  className="rounded-xl object-cover object-center h-80"
-                />
-              </AspectRatio>
-            </div>
-
-            <div className="uppercase text-gray-600 flex justify-between items-center text-xs md:text-sm  md:mb-4 mt-4 px-2">
-              <div
-                className="mr-2 mb-2 rounded-full border border-gray-600 hover:bg-transparent
-             text-gray-600 hover:text-gray-600 cursor-default text-xs md:text-sm px-5 py-1.5"
-              >
-                {blog.categories[0]}
+          <Link key={blog.title} href={``}>
+            <div className="group cursor-pointer">
+              <div>
+                <AspectRatio ratio={16 / 12}>
+                  <Image
+                    src={blog.imageUrl}
+                    fill
+                    alt="blog_img"
+                    className="rounded-xl object-cover object-center h-80"
+                  />
+                </AspectRatio>
               </div>
-              <p>
-                {new Date(blog._creationTime).toLocaleDateString("en-US", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                })}
-              </p>
-            </div>
 
-            <h1 className="text-lg md:text-2xl mb-3 md:mb-6 group-hover:underline px-2">
-              {blog.title}
-            </h1>
-          </div>
+              <div className="uppercase text-gray-600 flex justify-between items-center text-xs md:text-sm  md:mb-4 mt-4 px-2">
+                <div
+                  className="mr-2 mb-2 rounded-full border border-gray-600 hover:bg-transparent
+             text-gray-600 hover:text-gray-600 cursor-default text-xs md:text-sm px-5 py-1.5"
+                >
+                  {blog.categories[0]}
+                </div>
+                <p>
+                  {new Date(blog._creationTime).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </p>
+              </div>
+
+              <h1 className="text-lg md:text-2xl mb-3 md:mb-6 group-hover:underline px-2">
+                {blog.title}
+              </h1>
+            </div>
+          </Link>
         ))}
       </div>
     </div>

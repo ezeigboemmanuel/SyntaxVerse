696820b598afa9d52d1a5e3b89972ea52b4bf8cc
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-09-24 10:00:46 +0100

    Feat: metadata and seo for single page blog

--- app/blog/[blogId]/page.tsx
+++ app/blog/[blogId]/page.tsx
@@ -1,4 +1,30 @@
+import { Id } from "@/convex/_generated/dataModel";
 import SinglePageBlog from "./_components/SinglePageBlog";
+import { fetchQuery } from "convex/nextjs";
+import { api } from "@/convex/_generated/api";
+
+export async function generateMetadata({ params }: { params: { blogId: Id<"blogs"> } }) {
+  const blog = await fetchQuery(api.blogs.getSingleBlog, {
+    id: params.blogId,
+  });
+
+  return {
+    metadataBase: new URL("https://syntaxverse.vercel.app"),
+    title: blog.title,
+    description: blog.article.slice(0, 100),
+    openGraph: {
+      title: blog.title,
+      description: blog.article.slice(0, 100),
+      url: `https://syntaxverse.vercel.app/blog/${blog._id}`,
+      siteName: "SyntaxVerse",
+      images: [
+        {
+          url: blog.imageUrl,
+        },
+      ],
+    },
+  };
+}
 
 const BlogPage = () => {
   return (

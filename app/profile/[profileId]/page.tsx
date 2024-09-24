442aebed3220bcb8757a7257ba3e45364c695154
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-09-24 10:06:02 +0100

    Feat: metadata and seo for profile page

--- app/profile/[profileId]/page.tsx
+++ app/profile/[profileId]/page.tsx
@@ -5,6 +5,29 @@ import { Id } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 
+export async function generateMetadata({ params }: { params: { profileId: Id<"users"> } }) {
+  const user = await fetchQuery(api.users.getUserById, {
+    id: params.profileId,
+  });
+
+  return {
+    metadataBase: new URL("https://syntaxverse.vercel.app"),
+    title: user?.name || "SyntaxVerse",
+    description: user?.bio || "SyntaxVerse is a multi-user blog where different users can write and publish their blogs.",
+    openGraph: {
+      title: user?.name || "SyntaxVerse",
+      description: user?.bio || "SyntaxVerse is a multi-user blog where different users can write and publish their blogs.",
+      url: `https://syntaxverse.vercel.app/profile/${user?._id}`,
+      siteName: "SyntaxVerse",
+      images: [
+        {
+          url: user?.imageUrl,
+        },
+      ],
+    },
+  };
+}
+
 const ProfilePage = async ({
   params,
 }: {
